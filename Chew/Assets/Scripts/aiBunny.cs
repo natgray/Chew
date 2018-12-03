03bd86f19a3fa5ba6c7d50f21f40f489f538d21c
Nicco <nengbeck@csumb.edu>
2018-12-03 14:38:43 -0800

    furtherTinkering
    Added aiBunny capsules and a placeholder audio track

---
+++ Chew/Assets/Scripts/aiBunny.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class aiBunny : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    private Transform chewable;
+    float distance;
+
+    //public GameObject[] chewables;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (chewable == null)
+        {
+            chewable = GameObject.FindWithTag("chewable").transform;
+        }
+
+        //chewables = GameObject.FindWithTag("AIBunny").transform;
+
+
+        agent.SetDestination(chewable.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(chewable.transform.position, transform.position);
+        if (distance < 10)
+        {
+            //animate.SetTrigger("attack");
+        }
+        if (distance > 10)
+        {
+            //animate.SetTrigger("walk");
+        }
+        agent.SetDestination(chewable.transform.position);
+    }
+}
