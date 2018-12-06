a87480085f4b36e114d1ff25c78fd99b944aa5f0
Nicco <nengbeck@csumb.edu>
2018-12-06 12:07:19 -0800

    Ai's complete
    Human changes who it's chasing, and Bunnies choose random locations to attack

--- Chew/Assets/Scripts/aiBunny.cs
+++ Chew/Assets/Scripts/aiBunny.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
 public class aiBunny : MonoBehaviour
 {
@@ -9,12 +10,23 @@ public class aiBunny : MonoBehaviour
     public NavMeshAgent agent;
     private Transform chewable;
     float distance;
+    public GameObject[] arr;
+    float timeLeft = 5;
+    public int objNumber;
 
     //public GameObject[] chewables;
     // Use this for initialization
     void Start()
     {
+        arr = GameObject.FindGameObjectsWithTag("chewable");
+        
+     
+
         agent = GetComponent<NavMeshAgent>();
+
+        objNumber = UnityEngine.Random.Range(0, arr.Length);
+
+        chewable = arr[objNumber].transform;      
 
         if (chewable == null)
         {
@@ -22,8 +34,6 @@ public class aiBunny : MonoBehaviour
         }
 
         //chewables = GameObject.FindWithTag("AIBunny").transform;
-
-
         agent.SetDestination(chewable.transform.position);
 
     }
@@ -32,9 +42,17 @@ public class aiBunny : MonoBehaviour
     void Update()
     {
         distance = Vector3.Distance(chewable.transform.position, transform.position);
-        if (distance < 10)
+        if (distance < 5)
         {
             //animate.SetTrigger("attack");
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                objNumber = UnityEngine.Random.Range(0, arr.Length);
+                chewable = arr[objNumber].transform;
+                agent.SetDestination(chewable.transform.position);
+                timeLeft = 5;
+            }
         }
         if (distance > 10)
         {
--- Chew/Assets/Scripts/aiScript.cs
+++ Chew/Assets/Scripts/aiScript.cs
@@ -10,24 +10,45 @@ public class aiScript : MonoBehaviour {
     //Animator animate;
     float distance;
     int health = 0;
+    public GameObject[] arr;
+    public int objNumber;
+    float timeLeft = 5;
+
 
     // Use this for initialization
     void Start () {
         //animate = GetComponent<Animator>();
         //animate.SetTrigger("walk");
+
+        arr = GameObject.FindGameObjectsWithTag("Player");
+        objNumber = UnityEngine.Random.Range(0, arr.Length);
+        Player = arr[objNumber].transform;
+
+
+
         agent = GetComponent<NavMeshAgent>();
-        Player = GameObject.FindWithTag("Player").transform;
+        //Player = GameObject.FindWithTag("Player").transform;
         agent.SetDestination(Player.position);
         //col = GameObject.FindWithTag("col");
         //Collider collider = GetChild(0).GetComponent<Collider>();
+
+
     }
 
     // Update is called once per frame
     void Update () {
         distance = Vector3.Distance(Player.position, transform.position);
-        if (distance < 10)
+        if (distance < 5)
         {
-            //animate.SetTrigger("attack");
+            
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                objNumber = UnityEngine.Random.Range(0, arr.Length);
+                Player = arr[objNumber].transform;
+                agent.SetDestination(Player.transform.position);
+                timeLeft = 5;
+            }
         }
         if (distance > 10)
         {
