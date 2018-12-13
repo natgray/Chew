8bbd34aec0c85af1ff2e5b9a1d463b29709bc7d0
Nicco <nengbeck@csumb.edu>
2018-12-13 01:25:32 -0800

    Script and texture edits
    added materials and textures, as well as models and added to ai scripts

--- Chew/Assets/Scripts/aiBunny.cs
+++ Chew/Assets/Scripts/aiBunny.cs
@@ -8,19 +8,29 @@ public class aiBunny : MonoBehaviour
 
     public NavMeshAgent agent;
     private Transform chewable;
-    float distance;
+    private Transform enemyDistance;
+    public Transform Player1;
+    float chewPointDistance;
+    float enemydistance;
     public GameObject[] arr;
     float timeLeft = 5;
     public int objNumber;
+    public float health = 100;
+    public GameObject Bunny;
+    public MeshRenderer bunnyMesh;
+
+   
+
+   
 
     //public GameObject[] chewables;
     // Use this for initialization
     void Start()
     {
-        arr = GameObject.FindGameObjectsWithTag("chewable");
         
-     
-
+        arr = GameObject.FindGameObjectsWithTag("chewable");
+        //enemyHuman = GameObject.FindGameObjectWithTag("enemy");
+       
         agent = GetComponent<NavMeshAgent>();
 
         objNumber = UnityEngine.Random.Range(0, arr.Length);
@@ -32,6 +42,13 @@ public class aiBunny : MonoBehaviour
             chewable = GameObject.FindWithTag("chewable").transform;
         }
 
+        if (enemyDistance == null)
+        {
+            enemyDistance = GameObject.FindWithTag("enemy").transform;
+        }
+      
+      
+
         //chewables = GameObject.FindWithTag("AIBunny").transform;
         agent.SetDestination(chewable.transform.position);
 
@@ -40,23 +57,37 @@ public class aiBunny : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		arr = GameObject.FindGameObjectsWithTag("chewable");
-        distance = Vector3.Distance(chewable.transform.position, transform.position);
-        if (distance < 5)
+        arr = GameObject.FindGameObjectsWithTag("chewable");
+        chewPointDistance = Vector3.Distance(chewable.transform.position, transform.position);
+        enemydistance = Vector3.Distance(enemyDistance.transform.position, transform.position);
+        bunnyMesh = GetComponent<MeshRenderer>();
+        if (chewPointDistance < 5)
         {
             //animate.SetTrigger("attack");
             timeLeft -= Time.deltaTime;
             if (timeLeft < 0)
             {
-				chewable = arr[Random.Range(0, arr.Length)].transform;
+                chewable = arr[Random.Range(0, arr.Length)].transform;
                 agent.SetDestination(chewable.transform.position);
                 timeLeft = 5;
             }
         }
-        if (distance > 10)
+        if (chewPointDistance > 10)
         {
             //animate.SetTrigger("walk");
         }
-        agent.SetDestination(chewable.transform.position);
+
+        if (enemydistance < 1)
+        {
+            health = health - 0.25f;
+        }
+        if (health == 0)
+        {
+            bunnyMesh.enabled = false;
+            //Destroy(Bunny);
+
+        }
+
     }
+        
 }
--- Chew/Assets/Scripts/aiScript.cs
+++ Chew/Assets/Scripts/aiScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class aiScript : MonoBehaviour {
     private Transform Player;
+    public Transform Player1;
+    public GameObject PlayerLocation;
     public NavMeshAgent agent;
 
     //Animator animate;
@@ -23,38 +25,50 @@ public class aiScript : MonoBehaviour {
         arr = GameObject.FindGameObjectsWithTag("Player");
         objNumber = UnityEngine.Random.Range(0, arr.Length);
         Player = arr[objNumber].transform;
-
+        PlayerLocation = GameObject.FindGameObjectWithTag("Player");
 
 
         agent = GetComponent<NavMeshAgent>();
         //Player = GameObject.FindWithTag("Player").transform;
-        agent.SetDestination(Player.position);
+        agent.SetDestination(Player.transform.position);
         //col = GameObject.FindWithTag("col");
         //Collider collider = GetChild(0).GetComponent<Collider>();
-
+        distance = Vector3.Distance(Player.position, transform.position);
 
     }
 
     // Update is called once per frame
     void Update () {
         distance = Vector3.Distance(Player.position, transform.position);
+      
         if (distance < 5)
         {
-            
             timeLeft -= Time.deltaTime;
-            if (timeLeft < 0)
-            {
-                objNumber = UnityEngine.Random.Range(0, arr.Length);
-                Player = arr[objNumber].transform;
-                agent.SetDestination(Player.transform.position);
-                timeLeft = 5;
-            }
+            Debug.Log("doing Damage");
         }
-        if (distance > 10)
+        if (timeLeft < 0)
         {
-            //animate.SetTrigger("walk");
+            agent.SetDestination(Player.transform.position);
+            timeLeft = 5;
+            Debug.Log("Picking new target");
         }
-        agent.SetDestination(Player.position);
+
+        if (timeLeft > 0 && timeLeft < 5 && PlayerLocation)
+        {
+            //agent.SetDestination(Player.transform.position);
+            /* objNumber = UnityEngine.Random.Range(0, arr.Length);
+             Player = arr[objNumber].transform;
+             agent.SetDestination(Player.transform.position);
+             timeLeft = 5;*/
+            agent.SetDestination(Player1.transform.position);
+        }
+        
+        if (Player == null)
+        {
+            agent.SetDestination(Player1.transform.position);
+        }
+       
+
 
     }
 }
