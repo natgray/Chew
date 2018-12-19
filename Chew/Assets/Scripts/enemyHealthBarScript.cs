840a07cb4d431cf6e8d541ac573490ba8b1c1cdb
Nicco <nengbeck@csumb.edu>
2018-12-19 00:17:12 -0800

    added Healthbar

---
+++ Chew/Assets/Scripts/enemyHealthBarScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealthBarScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthBarScript.health -= 10f;
+    }
+}
