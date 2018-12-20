17ef8a0dfdf6779f1e135b6a080ae718c19d5925
Nicco <nengbeck@csumb.edu>
2018-12-19 19:51:33 -0800

    added bunny Models + simple Animation

---
+++ Chew/Assets/Scripts/player1BunnyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1BunnyMovement : MonoBehaviour {
+
+    Animator anim;
+
+    bool walking,running = false;
+    bool idle = true;
+
+   // int jumpHash = Animator.StringToHash("Jump");
+    //int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //anim.Play("Rab_Idle01", 0);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        /*if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isChewing", true);
+        }*/
+        else
+        {
+            anim.SetBool("isRunning", false);
+            //anim.SetBool("isChewing", false);
+        }
+
+    }
+}
